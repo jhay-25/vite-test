361af81651d067bbd7129186ff0a2c6b324229f4
John Carlo Cunanan <codingjohn@outlook.com>
2021-09-18 14:32:29 +0800

    finish horizontal gsap

--- src/pages/ScHorizontal.jsx
+++ src/pages/ScHorizontal.jsx
@@ -7,27 +7,28 @@ import Layout from "../components/Layout";
 function ScHorizontal() {
   gsap.registerPlugin(ScrollTrigger);
 
-  // useEffect(() => {
-  //   const sections = gsap.utils.toArray(".panel");
+  useEffect(() => {
+    const sections = gsap.utils.toArray(".panel");
 
-  //   gsap.to(sections, {
-  //     xPercent: -100 * (sections.length - 1),
-  //     ease: "none",
-  //     scrollTrigger: {
-  //       trigger: ".container",
-  //       pin: true,
-  //       scrub: 1,
-  //       markers: true,
-  //       snap: 1 / (sections.length - 1),
-  //       // base vertical scrolling on how wide the container is so it feels more natural.
-  //       end: () => "+=" + document.querySelector(".container").offsetWidth,
-  //     },
-  //   });
-  // }, []);
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".horContainer",
+        pin: true,
+        scrub: 1,
+        markers: true,
+
+        // base vertical scrolling on how wide the container is so it feels more natural.
+        end: () =>
+          "+=" + document.querySelector(".horContainer").offsetWidth / 4,
+      },
+    });
+  }, []);
 
   return (
     <Layout
-      className="flex flex-nowrap h-full container"
+      className="flex flex-nowrap h-full horContainer"
       style={{ width: "600vw", padding: "0" }}
     >
       <section className="w-screen h-screen flex justify-center items-center text-4xl text-white uppercase font-bold bg-blue-600 panel">
@@ -37,7 +38,7 @@ function ScHorizontal() {
         two
       </section>
 
-      {/* <section className="w-screen flex justify-center items-center text-4xl text-white uppercase font-bold bg-yellow-600 panel">
+      <section className="w-screen flex justify-center items-center text-4xl text-white uppercase font-bold bg-yellow-600 panel">
         three
       </section>
       <section className="w-screen flex justify-center items-center text-4xl text-white uppercase font-bold bg-green-600 panel">
@@ -48,7 +49,7 @@ function ScHorizontal() {
       </section>
       <section className="w-screen flex justify-center items-center text-4xl text-white uppercase font-bold bg-indigo-600 panel">
         six
-      </section> */}
+      </section>
     </Layout>
   );
 }
